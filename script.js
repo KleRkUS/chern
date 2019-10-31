6a6a7aa52dc2bafbddb19c6cccbfd8dc4fe47821
KleRkUs <villevald.vladislav.sleep@gmail.com>
2019-10-31 19:16:19 +0300

    da blya

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const carousel = document.getElementById('carousel'),
 	  carouselButtons = document.getElementsByClassName('carousel-button'),
-	  items = carousel.getElementsByClassName('assorti__card'),
-	  itemWidth = document.getElementById('card').offsetWidth;
-let index = 0;
+	  items = carousel.getElementsByClassName('assorti__card');
+let index = 0,
+	itemWidth = document.getElementById('card').offsetWidth;
 
 
 window.onload = function() {
@@ -17,11 +17,13 @@ window.onload = function() {
 
 function carouselScroll(buttIndex) {
 
+	itemWidth = document.getElementById('card').offsetWidth;
+
 	if ((buttIndex == "-1" && index == 0) || (buttIndex == "1" && index == (items.length - 1))) {
 		return 0;
 	}
 
 	index += Number(buttIndex);
-	carousel.style.right = index*itemWidth + 'px';
+	carousel.style.right = index*100 + '%';
 
 }
